a2a1a7da1f5e2b6580a30e73dcc9356b705dad1a
Christian Ray Leovido <18484997+leovido@users.noreply.github.com>
2024-12-02 18:11:03 +0000

    chore: rebase

--- apps/next/next.config.js
+++ apps/next/next.config.js
@@ -1,12 +1,12 @@
-const withPWA = require('next-pwa')({
-    dest: 'public',
-    disable: process.env.NODE_ENV === 'development',
-  });
-  
-  const nextConfig = withPWA({
-    experimental: {
-      appDir: true,
-    },
-  });
-  
-  module.exports = nextConfig;
+const withPWA = require("next-pwa")({
+  dest: "public",
+  disable: false,
+});
+
+const nextConfig = withPWA({
+  experimental: {
+    appDir: true,
+  },
+});
+
+module.exports = nextConfig;
