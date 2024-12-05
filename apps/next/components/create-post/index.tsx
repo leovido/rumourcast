30d0c787acef2ef94990fa82c00905d79bbfcbbc
Diskyeth <evert@evertmartin.be>
2024-12-05 22:28:04 +0100

    6

--- apps/next/components/create-post/index.tsx
+++ apps/next/components/create-post/index.tsx
@@ -530,7 +530,7 @@ function Channel() {
     <img
       src={channel.image_url}
       alt={channel.name}
-      className="rounded-full w-6 h-6 object-cover"
+      className="rounded-full w-8 h-8 object-cover"
     />
   ) : (
     <SquareSlash className="w-6 h-6" />
