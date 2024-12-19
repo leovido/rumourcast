bca0d5def4df49254f04a9018d5bfffb2c5cb4b6
Christian Ray Leovido <18484997+leovido@users.noreply.github.com>
2024-12-19 23:22:31 +0000

    fix

--- ecosystem.config.js
+++ ecosystem.config.js
@@ -3,20 +3,12 @@ module.exports = {
     {
       name: "rumourcast-api",
       script: "bun",
-      args: "run api:start", // or your API start script
+      args: "run index.ts",
+      cwd: "packages/api/src",
       watch: false,
       env: {
         NODE_ENV: "production",
         PORT: 3001,
-      },
-    },
-    {
-      name: "rumourcast-queue",
-      script: "bun",
-      args: "run queue:start", // or your queue start script
-      watch: false,
-      env: {
-        NODE_ENV: "production",
       },
     },
   ],
