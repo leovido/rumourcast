436e7f6f4ea3a2c04e5ddd27a95a928d2aaa5d4f
Christian Ray Leovido <18484997+leovido@users.noreply.github.com>
2024-12-20 07:35:15 +0000

    chore: pm2

--- ecosystem.config.js
+++ ecosystem.config.js
@@ -1,14 +1,34 @@
+const dotenv = require('dotenv');
+const path = require('path');
+
+const env = dotenv.config({
+  path: path.resolve(__dirname, 'packages/api/.env')
+}).parsed;
+
 module.exports = {
   apps: [
     {
       name: "rumourcast-api",
-      script: "./packages/api/src/index.ts",
-      interpreter: "bun",
+      script: "/root/.bun/bin/bun",
+      args: "run packages/api/src/index.ts",
+      cwd: "/root/rumourcast",
       watch: false,
       env: {
         NODE_ENV: "production",
         PORT: 3001,
+        ...env,
       },
     },
+    {
+      name: "rumourcast-updates",
+      script: "/root/.bun/bin/bun",
+      args: "run packages/api/scripts/updates.ts",
+      cwd: "/root/rumourcast",
+      watch: false,
+      env: {
+        NODE_ENV: "production",
+        ...env,
+      },
+    }
   ],
 };
