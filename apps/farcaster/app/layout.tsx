76ade17061d16a5715c5f9edaf4319b173d12b33
Diskyeth <evert@evertmartin.be>
2025-01-15 22:59:27 +0100

    123

--- apps/farcaster/app/layout.tsx
+++ apps/farcaster/app/layout.tsx
@@ -10,30 +10,10 @@ import HamburgerMenuButton from '@/components/ui/hamburger-menu-button'
 
 
 export const metadata: Metadata = {
-  title: 'anoncast',
-  description: 'Post anonymously to Farcaster.',
-  openGraph: {
-    title: 'anoncast',
-    description: 'Post anonymously to Farcaster.',
-    images: ['/anon.png'],
-  },
-  other: {
-    ['fc:frame']: JSON.stringify({
-      version: 'next',
-      imageUrl: 'https://anoncast.org/banner.png',
-      button: {
-        title: 'Post anonymously',
-        action: {
-          type: 'launch_frame',
-          name: 'anoncast',
-          url: 'https://rumourcast-frame.vercel.app/',
-          splashImageUrl: 'https://anoncast.org/anon.png',
-          splashBackgroundColor: '#151515',
-        },
-      },
-    }),
-  },
-}
+  title: "Yoink!",
+  description: "Click to yoink the flag.",
+};
+
 
 export default function RootLayout({
   children,
--- apps/farcaster/app/page.tsx
+++ apps/farcaster/app/page.tsx
@@ -4,6 +4,18 @@ import ActionComponent from '@/components/action'
 import { PostFeed, PromotedFeed } from '@/components/post-feed'
 import { CreatePostProvider, useCreatePost } from '@/components/create-post/context'
 
+ const metadata: Metadata = {
+  title: 'anoncast',
+  description: 'Post anonymously to Farcaster.',
+  openGraph: {
+    title: 'anoncast',
+    description: 'Post anonymously to Farcaster.',
+    images: ['/anon.png'],
+  },
+  other: {
+    
+      },
+    }
 
 export default function Home() {
   return (
@@ -13,31 +25,7 @@ export default function Home() {
   )
 }
 
-export const metadata: Metadata = {
-  title: 'anoncast',
-  description: 'Post anonymously to Farcaster.',
-  openGraph: {
-    title: 'anoncast',
-    description: 'Post anonymously to Farcaster.',
-    images: ['/anon.png'],
-  },
-  other: {
-    ['fc:frame']: JSON.stringify({
-      version: 'next',
-      imageUrl: 'https://anoncast.org/banner.png',
-      button: {
-        title: 'Post anonymously',
-        action: {
-          type: 'launch_frame',
-          name: 'anoncast',
-          url: 'https://rumourcast-frame.vercel.app/',
-          splashImageUrl: 'https://anoncast.org/anon.png',
-          splashBackgroundColor: '#151515',
-        },
-      },
-    }),
-  },
-}
+
 
 function Inner() {
   const { variant } = useCreatePost()
