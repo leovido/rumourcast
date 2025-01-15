9def8ea04d36cb19e02a0e4e59c14c0ffafacb5f
Diskyeth <evert@evertmartin.be>
2025-01-15 22:16:47 +0100

    Update page.tsx

--- apps/farcaster/app/page.tsx
+++ apps/farcaster/app/page.tsx
@@ -3,8 +3,6 @@
 import ActionComponent from '@/components/action'
 import { PostFeed, PromotedFeed } from '@/components/post-feed'
 import { CreatePostProvider, useCreatePost } from '@/components/create-post/context'
-import { sdk } from '@farcaster/frame-sdk'
-import dynamic from 'next/dynamic';
 
 export default function Home() {
   return (
