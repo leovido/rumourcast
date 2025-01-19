50d3791637a33722f33fe1504a2236ada04dc098
Samuel Huber <samu@smashice.de>
2025-01-19 03:51:59 +0100

    increase timeout

--- packages/api/src/utils.ts
+++ packages/api/src/utils.ts
@@ -36,7 +36,7 @@ export const createElysia = (config?: ConstructorParameters<typeof Elysia>[0]) =
       port: 3001,
       hostname: '0.0.0.0',
       development: process.env.NODE_ENV !== 'production',
-      idleTimeout: 10,
+      idleTimeout: 120,
     })
 
 export const augmentCasts = async (casts: Cast[]) => {
